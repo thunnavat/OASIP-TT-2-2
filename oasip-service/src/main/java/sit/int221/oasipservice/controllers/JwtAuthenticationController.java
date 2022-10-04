85684d1d68c7eae058c6c92f281de89dc61d002c
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2022-10-04 15:50:26 +0700

    refresh expired return status

--- oasip-service/src/main/java/sit/int221/oasipservice/controllers/JwtAuthenticationController.java
+++ oasip-service/src/main/java/sit/int221/oasipservice/controllers/JwtAuthenticationController.java
@@ -1,6 +1,7 @@
 package sit.int221.oasipservice.controllers;
 
 import io.jsonwebtoken.impl.DefaultClaims;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -9,6 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.server.ResponseStatusException;
 import sit.int221.oasipservice.config.JwtTokenUtil;
 import sit.int221.oasipservice.dtos.JwtRequest;
 import sit.int221.oasipservice.dtos.JwtResponse;
@@ -59,10 +61,13 @@ public class JwtAuthenticationController {
     public ResponseEntity<?> refreshtoken(HttpServletRequest request) throws Exception {
         // From the HttpRequest get the claims
         DefaultClaims claims = (io.jsonwebtoken.impl.DefaultClaims) request.getAttribute("claims");
-
-        Map<String, Object> expectedMap = getMapFromIoJsonwebtokenClaims(claims);
-        String token = jwtTokenUtil.doGenerateRefreshToken(expectedMap, expectedMap.get("sub").toString());
-        return ResponseEntity.ok(new JwtResponse(token));
+        if(claims == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "token has expired");
+        } else {
+            Map<String, Object> expectedMap = getMapFromIoJsonwebtokenClaims(claims);
+            String token = jwtTokenUtil.doGenerateRefreshToken(expectedMap, expectedMap.get("sub").toString());
+            return ResponseEntity.ok(new JwtResponse(token));
+        }
     }
 
     public Map<String, Object> getMapFromIoJsonwebtokenClaims(DefaultClaims claims) {
