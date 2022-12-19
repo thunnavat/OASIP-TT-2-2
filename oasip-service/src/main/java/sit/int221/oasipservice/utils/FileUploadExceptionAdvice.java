f8a4ae70d1c4afeeefe9f6e64ed6234227ede615
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2022-12-19 22:31:14 +0700

    handler exception file size

--- oasip-service/src/main/java/sit/int221/oasipservice/utils/FileUploadExceptionAdvice.java
+++ oasip-service/src/main/java/sit/int221/oasipservice/utils/FileUploadExceptionAdvice.java
@@ -1,15 +1,17 @@
 package sit.int221.oasipservice.utils;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-//@ControllerAdvice
-//public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
-//
-//    @ExceptionHandler(MaxUploadSizeExceededException.class)
-//    public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
-//        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!"));
-//    }
-//}
+@ControllerAdvice
+public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage("The file size cannot be larger than 10 MB"));
+    }
+}
---
+++ oasip-service/src/main/java/sit/int221/oasipservice/utils/ResponseMessage.java
@@ -0,0 +1,11 @@
+package sit.int221.oasipservice.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
+public class ResponseMessage {
+    private String message;
+}
